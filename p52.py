aa1edec8f831564254751690f6076cb65c64763c
Bhargavi-student <bhargavi8260@gmail.com>
2026-02-26 10:50:37 +0530

    Added python practice programs

---
+++ p52.py
@@ -0,0 +1,11 @@
+class College:
+    college_name = "GIET"
+
+    def __init__(self, name):
+        self.name = name
+
+s1 = College("Bhargavi")
+s2 = College("Riya")
+
+print(s1.college_name)
+print(s2.college_name)
