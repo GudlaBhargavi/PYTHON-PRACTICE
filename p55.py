fc8db2984ccb9afc8e7029a3fad1ffe57d18ef7b
Bhargavi-student <bhargavi8260@gmail.com>
2026-02-26 11:04:30 +0530

    Added python practice programs

---
+++ p55.py
@@ -0,0 +1,18 @@
+def binary_search(arr, key):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+arr = [1, 2, 3, 4, 5]
+print(binary_search(arr, 4))
