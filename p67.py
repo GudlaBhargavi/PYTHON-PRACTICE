a7d8d06bd0322899ab089659069c655f231000da
Bhargavi-student <bhargavi8260@gmail.com>
2026-03-08 15:26:42 +0530

    Added python practice programs

---
+++ p67.py
@@ -0,0 +1,20 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+
+print(s.pop())
