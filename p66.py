21fb9468462a099c95b3e99f0f477503f3384a26
Bhargavi-student <bhargavi8260@gmail.com>
2026-03-08 15:24:40 +0530

    Added to python practice programs

---
+++ p66.py
@@ -0,0 +1,13 @@
+stack = []
+
+# Push elements into stack
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack:", stack)
+
+# Pop element from stack
+stack.pop()
+
+print("After pop:", stack)
